bb7918bd4ac4bcdd591b9519890865e136e57b60
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-07-22 09:38:18 +0200

    added required packages to test if host can install and run

---
+++ src/mp3backend/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import fastapi
+import requests
+import pytube
+import moviepy
+import bs4
+import youtubesearchpython
+
+
+api = fastapi.FastAPI()
