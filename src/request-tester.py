521c5100c07624f6bc7613be5ed116a2c51db790
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-07-23 22:45:53 +0200

    added websocket test

--- src/request-tester.py
+++ src/request-tester.py
@@ -4,15 +4,36 @@ r"""
 
 """
 from pprint import pprint
+import json
 import requests
+import asyncio
+import websockets
+import websockets.legacy.client
 
 
-url = r""
+ID = "ha-edv6PVqY"
+url = f"ws://127.0.0.1:5000/api/download/{ID}"
 payload = dict(
-
+    metadata=dict(
+        title="I'M ALRIGHT!",
+        artist="Sportfreunde Stiller"
+    )
 ) or None
 
-response = requests.get(url, json=payload)
 
-print(response.status_code)
-pprint(response.json())
+async def main():
+    async with websockets.connect(url) as websocket:
+        websocket: websockets.legacy.client.WebSocketClientProtocol
+        await websocket.send(json.dumps(payload))
+        while not websocket.closed:
+            message = await websocket.recv()
+            data = json.loads(message)
+            pprint(data)
+
+
+asyncio.run(main())
+
+
+# response = requests.get(url, json=payload)
+# print(response.status_code)
+# pprint(response.json())
