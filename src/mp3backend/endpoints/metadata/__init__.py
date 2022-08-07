215c438907061ee69e3ec9aeb7d707ffc545b881
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-08-07 16:17:00 +0200

    added exception checks

--- src/mp3backend/endpoints/metadata/__init__.py
+++ src/mp3backend/endpoints/metadata/__init__.py
@@ -3,8 +3,9 @@
 r"""
 
 """
-from fastapi import Query
+from fastapi import Query, HTTPException, status
 from pytube import YouTube
+from pytube.exceptions import VideoUnavailable, RegexMatchError
 from main import api
 from . import utility
 from . import models
@@ -19,7 +20,13 @@ def metadata(
         youtubeId: str = Query()
 ):
     url = utility.completeYoutubeUrl(known=youtubeId)
-    video = YouTube(url)
+    try:
+        video = YouTube(url)
+        video.check_availability()
+    except RegexMatchError:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="invalid youtubeId passed")
+    except VideoUnavailable:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="video not found")
     return dict(
         video_id=video.video_id,
         title=video.title,
@@ -45,4 +52,7 @@ def metadata2(
         youtubeId: str = Query()
 ):
     from youtubesearchpython import Video
-    return Video.get(youtubeId, timeout=20)
+    try:
+        return Video.get(youtubeId, timeout=20)
+    except TypeError:  # attempts to add something that can return None to a string
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "video not found")
